7566b9ad09a03f19b81a2f5faf9690b0aea450a9
kyledobash <kyledobash@yahoo.com>
2020-12-10 13:40:37 -0700

    Created Game class, Player parent class, and HumanPlayer and Computer Player child classes. Added Skeletons to each respectively.

---
+++ RSPLS_actualproj_KD/RSPLS_actualproj_KD/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSPLS_actualproj_KD
+{
+    public class Game
+    {
+        //MEMBER VARIABLES (WHAT IT HAS)
+
+        //CONSTRUCTOR (SPAWNER)
+
+        //MEMBER METHODS (WHAT IT CAN DO)
+
+    }
+}
