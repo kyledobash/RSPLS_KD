11f3e60ece06dfec279adcad933145294f676952
kyledobash <kyledobash@yahoo.com>
2020-12-11 11:06:43 -0700

    Created CompareGestures and ShowScore methods in the Game class.

--- RSPLS_actualproj_KD/RSPLS_actualproj_KD/Game.cs
+++ RSPLS_actualproj_KD/RSPLS_actualproj_KD/Game.cs
@@ -63,12 +63,120 @@ namespace RSPLS_actualproj_KD
             }
         }
 
+        public void ShowScore()
+        {
+            Console.WriteLine("Player 1: " + players[0].score);
+            Console.WriteLine("Player 1: " + players[1].score);
+        }
+
         public void gestureCompare(string player1gesture, string player2gesture, Player player1, Player player2)
         {
-            if (player1gesture == "Rock" && player2gesture == "Scissors" )
+            //Player 1 round victory cases
+            if (player1gesture == player1.gestures[0] && player2gesture == player2.gestures[2])
             {
-                Console.WriteLine(player1gesture + " crushes " + player2gesture);
-                playerOne.score++
+                Console.WriteLine("Player 1's " + player1gesture + " crushes Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[2] && player2gesture == player2.gestures[1])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " cuts Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[1] && player2gesture == player2.gestures[0])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " covers Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[0] && player2gesture == player2.gestures[3])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " crushes Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[3] && player2gesture == player2.gestures[4])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " poisons Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[4] && player2gesture == player2.gestures[2])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " smashes Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[2] && player2gesture == player2.gestures[3])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " decapitates Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[3] && player2gesture == player2.gestures[1])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " eats Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[1] && player2gesture == player2.gestures[4])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " disproves Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            else if (player1gesture == player1.gestures[4] && player2gesture == player2.gestures[0])
+            {
+                Console.WriteLine("Player 1's " + player1gesture + " vaporizes Player 2's " + player2gesture + "!");
+                player1.IncrementScore();
+            }
+            //player 2 round victory cases
+            else if (player2gesture == player2.gestures[0] && player1gesture == player1.gestures[2])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " crushes Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[2] && player1gesture == player1.gestures[1])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " cuts Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[1] && player1gesture == player1.gestures[0])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " covers Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[0] && player1gesture == player1.gestures[3])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " crushes Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[3] && player1gesture == player1.gestures[4])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " poisons Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[4] && player1gesture == player1.gestures[2])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " smashes Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[2] && player1gesture == player1.gestures[3])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " decapitates Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[3] && player1gesture == player1.gestures[1])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " eats Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[1] && player1gesture == player1.gestures[4])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " disproves Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            else if (player2gesture == player2.gestures[4] && player1gesture == player1.gestures[0])
+            {
+                Console.WriteLine("Player 2's " + player2gesture + " vaporizes Player 1's " + player1gesture + "!");
+                player2.IncrementScore();
+            }
+            //edge case
+            else
+            {
+                Console.WriteLine("Error comparing player gestures");
             }
         }
     }
